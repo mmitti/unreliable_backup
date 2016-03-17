d95bcfda56d6ad710194fa5f7bd6050e42b337f1
mmitti <mmitti.mail@gmail.com>
2016-03-17 22:29:28 +0900

    syntax error修正、デバッグメッセージ追加

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 class buckup_data:
     def __init__(self, cursol):
         self.dir = ""
-        self.file_data = []
+        self.file_data = {}
         self.child_dir = []
         self.is_new = True
         self.cursol = cursol
@@ -22,10 +22,11 @@ class buckup_data:
         if(not self.is_new):
             self.cursol.execute("""DELETE from backup_data WHERE dir=?;""", (self.dir))
 
-    def read(self, dir = self.dir):
+    def read(self, dir = None):
+        if dir is None:dir = self.dir
         self.cursol.execute("""SELECT * from backup_data;""")
-        data = cursol.fetchone()
-        if(data is( not None):
+        data = self.cursol.fetchone()
+        if(data is not None):
             self.dir = data.dir
             self.file_data = json.loads(data.file_data)
             self.child_dir = json.loads(data.child_dir)
@@ -39,7 +40,7 @@ class buckup_data:
     def add_file(self, name, fhash):
         self.file_data[name] = fhash
     def add_child(self, name):
-        if name in self.child_dir
+        if name in self.child_dir: return
         self.child_dir.append(name)
 
     def update_removed_files(self, exisits_files):
@@ -49,56 +50,82 @@ class buckup_data:
         return list(removed_files)
 
     def update_removed_dirs(self, exisits_dirs):
-        removed_dirs = set(self.child_dir.keys()) - set(exisits_dirs)
+        removed_dirs = set(self.child_dir) - set(exisits_dirs)
         for f in removed_dirs:
-            self.child_dir.pop(f)
+            self.child_dir.remove(f)
         return list(removed_dirs)
 
 
 def get_file_hash(file_path):
-    md5 = hashlib.md5()
-    with open(file_path, 'rb') as f:
-        for chunk in iter(lambda: f.read(2048 * md5.block_size), b''):
-            md5.update(chunk)
-    return md5.hexdigest()
+    try:
+        md5 = hashlib.md5()
+        with open(file_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(2048 * md5.block_size), b''):
+                md5.update(chunk)
+        return md5.hexdigest()
+    except OSError as e:
+         print("HASH I/O error({0}): {1}".format(e.errno, e.strerror))
+    return -1
 
 def file_exists(path):
-    return os.path.exists(path)
+    try:
+        return os.path.exists(path)
+    except OSError as e:
+         print("FILE_EXISTS I/O error({0}): {1}".format(e.errno, e.strerror))
+    return False
 
 def file_copy(s, d):
-    shutil.copy2(s,d)
-
+    try:
+        shutil.copy2(s,d)
+    except IOError as e:
+         print("FILE_CP I/O error({0}): {1}".format(e.errno, e.strerror))
 def file_remove(path):
-    os.remove(path)
+    try:
+        os.remove(path)
+    except OSError as e:
+         print("FILE_RM I/O error({0}): {1}".format(e.errno, e.strerror))
 def dir_remove(path):
-    shutil.rmtree(path)
-
+    try:
+        shutil.rmtree(path)
+    except IOError as e:
+         print("RM_TREE I/O error({0}): {1}".format(e.errno, e.strerror))
+def make_dirs(path):
+    try:
+        os.makedirs(path)
+    except IOError as e:
+         print("MK_DIR I/O error({0}): {1}".format(e.errno, e.strerror))
 # srcの中身を から dest へコピーする
 def exec_copy(src_dir, dest_dir, cursol):
-    data = buckup_data(src_dir)
-    data.read()
+    print("ENTER {0}".format(src_dir))
+    data = buckup_data(cursol)
+    data.read(src_dir)
     #srcにデータが存在しない場合
     #dbにデータが存在するなどの場合に起きうる
     if(not file_exists(src_dir)):
+        print("DIR DELETED")
         data.delete()
         if file_exists(dest_dir):
             dir_remove(dest_dir)
-        for f in data.child_dir.keys()
+        for f in data.child_dir:
             exec_copy(src_dir+'/'+f, dest_dir+'/'+f, cursol)
         return
     if(not file_exists(dest_dir)):
-        os.makedirs(dest_dir)
+        print("DEST MKDIR")
+        make_dirs(dest_dir)
         shutil.copystat(src_dir, dest_dir)
     exists_files = []
     exists_dirs = []
 
     #ファイルコピー
-
+    print(os.listdir(src_dir))
     for l in os.listdir(src_dir):
+
         new_src = src_dir+'/'+l
         new_dest = dest_dir+'/'+l
+        print("CP {0}".format(new_src))
         if os.path.islink(new_src):
-
+            #todo
+            i = 1
         elif os.path.isdir(new_src):
             exec_copy(new_src, new_dest, cursol)
             data.add_child(new_src)
@@ -106,35 +133,37 @@ def exec_copy(src_dir, dest_dir, cursol):
         else:
             fhash = get_file_hash(new_src)
             #ハッシュが違うorハッシュがない
-            if not fhash == data.get_file_hash(l)
+            if not fhash == data.get_file_hash(l):
                 file_copy(new_src, new_dest)
-                data.add_file(fhash)
+                data.add_file(l, fhash)
             exists_files.append(l)
 
+    print("RM FILES")
     #ソースに無く、destに残っているゴミも消すため再帰的に処理
     delete_list = data.update_removed_files(exists_files)
     for f in delete_list:
         file_remove(dest_dir+'/'+f)
 
+    print("RM DIRS")
     delete_list = data.update_removed_dirs(exists_dirs)
     for f in delete_list:
         exec_copy(src_dir+'/'+f, dest_dir+'/'+f, cursol)
-
+    print("EXIT {0}".format(src_dir))
 def backup(arg):
     parser = argparse.ArgumentParser(description='バックアップモード', usage='%(prog)s backup [-h] target [target ...] dest db [options..]')
     parser.add_argument('target', nargs='+', help='バックアップソースのディレクトリ')
-    parser.add_argument('dest', nargs=1, help='データ保存先')
-    parser.add_argument('db', nargs=1, help='検証用DB保存先。データ保存先と別の場所にしてください。')
+    parser.add_argument('dest', type=str, help='データ保存先')
+    parser.add_argument('db', type=str, help='検証用DB保存先。データ保存先と別の場所にしてください。')
     args = parser.parse_args(arg)
     conn = sqlite3.connect(args.db)
     cur = conn.cursor()
     try:
         cur.execute("""CREATE TABLE backup_data(dir text, file_data text, child_dir text);""")
     except:
+        print("A")
     for t in args.target:
         exec_copy(t, args.dest+'/'+os.path.basename(t), cur)
 
-    print(args)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='信用できないストレージにバックアップするツール。\n気が向いたらエラー訂正も入れる。')
