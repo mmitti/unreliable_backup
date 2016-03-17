d318f03b7746b17b0f3098c1a6f851d682fd52bc
mmitti <mmitti.mail@gmail.com>
2016-03-17 23:06:33 +0900

    bug fix

--- main.py
+++ main.py
@@ -5,31 +5,33 @@ import os
 import shutil
 import json
 import hashlib
+import stat
 class buckup_data:
-    def __init__(self, cursol):
-        self.dir = ""
+    def __init__(self, cursol, dir = None):
+        self.dir = dir
+        if self.dir is None:self.dir = ""
         self.file_data = {}
         self.child_dir = []
         self.is_new = True
         self.cursol = cursol
     def save(self):
         if(self.is_new):
-            self.cursol.execute("""INSERT INTO backup_data(dir, file_data, child_dir) VALUES(?, ?, ?);""", (self.dir, json.dump(self.file_data), json.dump(self.child_dir)))
+            self.cursol.execute("""INSERT INTO backup_data(dir, file_data, child_dir) VALUES(?, ?, ?);""", (self.dir, json.dumps(self.file_data), json.dumps(self.child_dir)))
             self.is_new = False
         else:
-            self.cursol.execute("""UPDATE backup_data set file_data=?, child_dir=?  WHERE dir=?;""", (json.dump(self.file_data), json.dump(self.child_dir), self.dir))
+            self.cursol.execute("""UPDATE backup_data set file_data=?, child_dir=?  WHERE dir=?;""", (json.dumps(self.file_data), json.dumps(self.child_dir), self.dir))
     def delete(self):
         if(not self.is_new):
-            self.cursol.execute("""DELETE from backup_data WHERE dir=?;""", (self.dir))
+            self.cursol.execute("""DELETE from backup_data WHERE dir=?;""", (self.dir,))
 
     def read(self, dir = None):
         if dir is None:dir = self.dir
         self.cursol.execute("""SELECT * from backup_data;""")
         data = self.cursol.fetchone()
         if(data is not None):
-            self.dir = data.dir
-            self.file_data = json.loads(data.file_data)
-            self.child_dir = json.loads(data.child_dir)
+            self.dir = data[0]
+            self.file_data = json.loads(data[1])
+            self.child_dir = json.loads(data[2])
             self.is_new = False
     def get_file_hash(self, name):
         return self.file_data.get(name, 0)
@@ -77,6 +79,8 @@ def file_exists(path):
 def file_copy(s, d):
     try:
         shutil.copy2(s,d)
+        file_stat = os.stat(s)
+        os.chown(d, file_stat[stat.ST_UID], file_stat[stat.ST_GID])
     except IOError as e:
          print("FILE_CP I/O error({0}): {1}".format(e.errno, e.strerror))
 def file_remove(path):
@@ -96,9 +100,9 @@ def make_dirs(path):
          print("MK_DIR I/O error({0}): {1}".format(e.errno, e.strerror))
 # srcの中身を から dest へコピーする
 def exec_copy(src_dir, dest_dir, cursol):
-    print("ENTER {0}".format(src_dir))
-    data = buckup_data(cursol)
-    data.read(src_dir)
+    print("ENTER {0} ".format(src_dir))
+    data = buckup_data(cursol, src_dir)
+    data.read()
     #srcにデータが存在しない場合
     #dbにデータが存在するなどの場合に起きうる
     if(not file_exists(src_dir)):
@@ -113,6 +117,8 @@ def exec_copy(src_dir, dest_dir, cursol):
         print("DEST MKDIR")
         make_dirs(dest_dir)
         shutil.copystat(src_dir, dest_dir)
+        file_stat = os.stat(src_dir)
+        os.chown(dest_dir, file_stat[stat.ST_UID], file_stat[stat.ST_GID])
     exists_files = []
     exists_dirs = []
 
@@ -122,18 +128,19 @@ def exec_copy(src_dir, dest_dir, cursol):
 
         new_src = src_dir+'/'+l
         new_dest = dest_dir+'/'+l
-        print("CP {0}".format(new_src))
         if os.path.islink(new_src):
-            #todo
-            i = 1
+            file_copy(new_src, new_dest)
+            data.add_file(l, 0)
+            exists_files.append(l)
         elif os.path.isdir(new_src):
             exec_copy(new_src, new_dest, cursol)
-            data.add_child(new_src)
+            data.add_child(l)
             exists_dirs.append(l)
         else:
             fhash = get_file_hash(new_src)
             #ハッシュが違うorハッシュがない
             if not fhash == data.get_file_hash(l):
+                print("CP {0} #{1}".format(new_src, fhash))
                 file_copy(new_src, new_dest)
                 data.add_file(l, fhash)
             exists_files.append(l)
@@ -141,13 +148,16 @@ def exec_copy(src_dir, dest_dir, cursol):
     print("RM FILES")
     #ソースに無く、destに残っているゴミも消すため再帰的に処理
     delete_list = data.update_removed_files(exists_files)
+    print(delete_list)
     for f in delete_list:
         file_remove(dest_dir+'/'+f)
 
     print("RM DIRS")
     delete_list = data.update_removed_dirs(exists_dirs)
+    print(delete_list)
     for f in delete_list:
         exec_copy(src_dir+'/'+f, dest_dir+'/'+f, cursol)
+    data.save()
     print("EXIT {0}".format(src_dir))
 def backup(arg):
     parser = argparse.ArgumentParser(description='バックアップモード', usage='%(prog)s backup [-h] target [target ...] dest db [options..]')
@@ -163,7 +173,8 @@ def backup(arg):
         print("A")
     for t in args.target:
         exec_copy(t, args.dest+'/'+os.path.basename(t), cur)
-
+    conn.commit()
+    cur.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='信用できないストレージにバックアップするツール。\n気が向いたらエラー訂正も入れる。')
